0d5e1bd6231c1c9bdd641eefce09d049b41da243
Kokecena <jovnni.nariciso@gmail.com>
2024-04-12 13:22:52 -0600

    Se colocaron las validaciones necesarias para los DTO

--- src/main/java/com/github/kokecena/gestioncontactos/model/ContactoDTO.java
+++ src/main/java/com/github/kokecena/gestioncontactos/model/ContactoDTO.java
@@ -1,14 +1,24 @@
 package com.github.kokecena.gestioncontactos.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ContactoDTO {
     private InformacionDTO informacion;
     private DireccionDTO direccion;
+    @NotNull(message = "El correo no puede estar vacío")
+    @Email(message = "El formato del email no es valido", regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")
     private String correo;
+    @NotNull(message = "El telefono no puede estar vació")
+    @Pattern(regexp = "^[+]?[(]?\\d{3}[)]?[-\\s.]?\\d{3}[-\\s.]?\\d{4,6}$", message = "Debe ser un numero de telefono valido.")
     private String telefono;
 }
--- src/main/java/com/github/kokecena/gestioncontactos/model/DireccionDTO.java
+++ src/main/java/com/github/kokecena/gestioncontactos/model/DireccionDTO.java
@@ -1,16 +1,31 @@
 package com.github.kokecena.gestioncontactos.model;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class DireccionDTO {
+    @Max(value = 50, message = "Colonia solo puede tener hasta 50 caracteres")
     private String colonia;
+    @Max(value = 25, message = "Codigo postal solo puede tener hasta 25 caracteres")
     private String codigoPostal;
+    @Max(value = 100, message = "Primera calle solo puede tener hasta 100 caracteres")
     private String primeraCalle;
+    @Max(value = 100, message = "Segunda calle solo puede tener hasta 100 caracteres")
     private String segundaCalle;
-    private String numInterno;
-    private String numExterno;
+    @Max(value = 5)
+    @Pattern(regexp = "^((s/n)|(S/N)|(sn)|(SN)|(\\d+([A-Za-z])?)|(\\d+([A-Za-z])?-\\d+)|(\\d+))$",
+            message = "Número no válido")
+    private String numero;
+    @Max(value = 5)
+    @Pattern(regexp = "^((s/n)|(S/N)|(sn)|(SN)|(\\d+([A-Za-z])?)|(\\d+([A-Za-z])?-\\d+)|(\\d+))$",
+            message = "Número externo no válido")
+    private String numeroExterno;
 }
