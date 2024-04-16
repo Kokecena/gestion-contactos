9bb6bf7cd6b1336771d496de4c7721d20506a9be
Kokecena <jovnni.nariciso@gmail.com>
2024-04-16 09:00:37 -0600

    Controlador actualizado
    added: findByPhoneNumber y updateContacto
    changed: deleteContacto es implementado correctamente ahora
    changed: Se agregan validaciones ahora

--- src/main/java/com/github/kokecena/gestioncontactos/controller/ContactoController.java
+++ src/main/java/com/github/kokecena/gestioncontactos/controller/ContactoController.java
@@ -1,19 +1,22 @@
 package com.github.kokecena.gestioncontactos.controller;
 
 import com.github.kokecena.gestioncontactos.commons.response.Response;
-import com.github.kokecena.gestioncontactos.entity.Contacto;
 import com.github.kokecena.gestioncontactos.model.ContactoDTO;
 import com.github.kokecena.gestioncontactos.model.ContactoResponse;
+import com.github.kokecena.gestioncontactos.model.annotations.PhoneNumber;
 import com.github.kokecena.gestioncontactos.service.ContactoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/contactos")
 @RequiredArgsConstructor
+@Validated
 public class ContactoController {
 
     private final ContactoService contactoService;
@@ -23,14 +26,25 @@ public class ContactoController {
         return Response.success(contactoService.findAll());
     }
 
+    @GetMapping
+    public ResponseEntity<Response<ContactoDTO>> findByPhoneNumber(@RequestParam @PhoneNumber String telefono) {
+        return Response.success(contactoService.findBy(telefono));
+    }
+
     @PostMapping
-    public ResponseEntity<Response<ContactoResponse>> saveContacto(ContactoDTO contacto) {
+    public ResponseEntity<Response<ContactoResponse>> saveContacto(@Valid @RequestBody ContactoDTO contacto) {
         return Response.success(contactoService.saveContacto(contacto));
     }
 
+    @PutMapping("/{telefono}")
+    public ResponseEntity<Response<ContactoResponse>> updateContacto(@PathVariable @PhoneNumber String telefono,
+                                                                     @Valid @RequestBody ContactoDTO contacto) {
+        return Response.success(contactoService.updateContacto(telefono, contacto));
+    }
+
     @DeleteMapping
-    public ResponseEntity<Response<Contacto>> deleteContacto(ContactoDTO contacto) {
-        throw new IllegalStateException("No implementado XD");
+    public ResponseEntity<Response<ContactoResponse>> deleteContacto(@RequestParam @PhoneNumber String telefono) {
+        return Response.success(contactoService.deleteContacto(telefono));
     }
 
 }
