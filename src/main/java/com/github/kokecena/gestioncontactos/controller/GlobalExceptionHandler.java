cad0b2642de9212bdcf292850c56f50ce942adf2
Kokecena <jovnni.nariciso@gmail.com>
2024-04-16 15:28:29 -0600

    Añadido mas manejo de excepciones de servlet

--- src/main/java/com/github/kokecena/gestioncontactos/controller/GlobalExceptionHandler.java
+++ src/main/java/com/github/kokecena/gestioncontactos/controller/GlobalExceptionHandler.java
@@ -9,12 +9,15 @@ import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import javax.servlet.ServletException;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -33,9 +36,11 @@ public class GlobalExceptionHandler {
                 .build();
     }
 
-    @ExceptionHandler(NoHandlerFoundException.class)
+    @ExceptionHandler({NoHandlerFoundException.class,
+            HttpRequestMethodNotSupportedException.class,
+            MissingServletRequestParameterException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<ResponseMessage> handleNotFoundException(NoHandlerFoundException ex) {
+    public ResponseEntity<ResponseMessage> handleNotFoundException(ServletException ex) {
         return Response.of(HttpStatus.NOT_FOUND)
                 .message(ex.getMessage())
                 .build();
