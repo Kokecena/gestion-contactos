939f41a6539170b90fec75dd0391123fe7b9b8b5
Kokecena <jovnni.nariciso@gmail.com>
2024-04-16 08:55:40 -0600

    Se agregaron 2 metodos al repositorio
    added: deleteByNumTelefonno y findByNumTelefono

--- src/main/java/com/github/kokecena/gestioncontactos/repository/ContactoRepository.java
+++ src/main/java/com/github/kokecena/gestioncontactos/repository/ContactoRepository.java
@@ -3,5 +3,9 @@ package com.github.kokecena.gestioncontactos.repository;
 import com.github.kokecena.gestioncontactos.entity.Contacto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ContactoRepository extends JpaRepository<Contacto, Long> {
+    void deleteByNumTelefono(String numTelefono);
+    Optional<Contacto> findByNumTelefono(String numTelefono);
 }
