a4ce2bf8ca9107a62c69ff432d974a75df37e76a
Kokecena <jovnni.nariciso@gmail.com>
2024-04-16 15:28:49 -0600

    Cambio de NotNull a NotBlank

--- src/main/java/com/github/kokecena/gestioncontactos/model/annotations/PhoneNumber.java
+++ src/main/java/com/github/kokecena/gestioncontactos/model/annotations/PhoneNumber.java
@@ -3,11 +3,11 @@ package com.github.kokecena.gestioncontactos.model.annotations;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.ReportAsSingleViolation;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import java.lang.annotation.*;
 
-@NotNull
+@NotBlank
 @Pattern(regexp = "^[+]?[(]?\\d{3}[)]?[-\\s.]?\\d{3}[-\\s.]?\\d{4,6}$")
 @Target({ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
